3c0782b52598e1cf29e1e5d3f91113391d2ef291
Matteo <92362045+StitchMl@users.noreply.github.com>
2021-11-06 14:34:05 +0100

    Add popup window

--- main/java/com/example/sportify/Controller.java
+++ main/java/com/example/sportify/Controller.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javax.swing.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,19 +26,36 @@ public class Controller implements Initializable {
     private Button signUp, submitLogin, submitSignUp, skipLogin, skipSignUp, signIn, home;
 
     @FXML
-    protected void submitAction() {
+    protected void submitActionLogin() {
 
         String userValue = username.getText(); 								//get user entered username from the textField1
         String passValue = password.getText(); 								//get user entered password from the textField2
 
         //check whether the credentials are authentic or not
+        JFrame jFrame = new JFrame();
         if (userValue.equals("test1@gmail.com") && passValue.equals("test")){	//if authentic, navigate user to a new page
-
-            System.out.println("Correct!");
+            JOptionPane.showMessageDialog(jFrame, "Correct!");
         }
         else{
             //show error message
-            System.out.println("Please enter valid username and password");
+            JOptionPane.showMessageDialog(jFrame, "Please enter valid username and password");
+        }
+    }
+
+    @FXML
+    protected void submitActionSignUp() {
+
+        String userValue = username.getText(); 								//get user entered username from the textField1
+        String passValue = password.getText(); 								//get user entered password from the textField2
+
+        //check whether the credentials are authentic or not
+        JFrame jFrame = new JFrame();
+        if ( !userValue.equals("") && !passValue.equals("") && !firstName.equals("") && !lastName.equals("")){	//if authentic, navigate user to a new page
+            JOptionPane.showMessageDialog(jFrame, "Thank you for your registration!");
+        }
+        else{
+            //show error message
+            JOptionPane.showMessageDialog(jFrame, "Please enter all value.");
         }
     }
 
--- main/java/module-info.java
+++ main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.sportify {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires javafx.graphics;
+    requires java.desktop;
 
     opens com.example.sportify to javafx.fxml;
     exports com.example.sportify;
