99a356d4b5e138cd7addaaf70561a9e53a80b965
Matteo <92362045+StitchMl@users.noreply.github.com>
2021-11-15 15:50:45 +0100

    Correct some error

--- main/java/com/example/sportify/FindGymController.java
+++ main/java/com/example/sportify/FindGymController.java
@@ -3,21 +3,13 @@ package com.example.sportify;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FindGymController implements Initializable {
-
-    //TextField
-    @FXML
-    private TextField gymName;
-    @FXML
-    private TextField gymAddress;
-    @FXML
-    private TextField gymCity;
 
     //Button
     @FXML
@@ -25,13 +17,21 @@ public class FindGymController implements Initializable {
     @FXML
     private Button home;
     @FXML
-    private Button submitSignUpGym;
-    @FXML
-    private Button skipSignUpGym;
-    @FXML
     private Button sportQuiz;
     @FXML
     private Button findGym;
+
+    @FXML
+    protected void sportQuizAction() {
+        JFrame jFrame = new JFrame();
+        JOptionPane.showMessageDialog(jFrame, "You're in sport quiz form!");
+    }
+
+    @FXML
+    protected void findGymAction() {
+        JFrame jFrame = new JFrame();
+        JOptionPane.showMessageDialog(jFrame, "You try to find a Gym!");
+    }
 
     @FXML
     protected void signLoginAction() throws Exception {
@@ -42,20 +42,13 @@ public class FindGymController implements Initializable {
     private void homeAction() throws Exception {
         CreateWindow.homeAction(home);
     }
-    @FXML
-    private void skipAction() throws Exception {
-        CreateWindow.loginAction(skipSignUpGym);
-    }
+
     @FXML
     private void lightColor(MouseEvent event) {
         if (event.getSource() == signInGym) {
             HighlightButton.lightColor(signInGym);
         } else if (event.getSource() == home) {
             HighlightButton.lightColor(home);
-        } else if (event.getSource() == submitSignUpGym) {
-            HighlightButton.lightColor(submitSignUpGym);
-        } else if (event.getSource() == skipSignUpGym) {
-            HighlightButton.lightColor(skipSignUpGym);
         } else if (event.getSource() == sportQuiz) {
             HighlightButton.lightColor(sportQuiz);
         } else if (event.getSource() == findGym) {
@@ -69,10 +62,6 @@ public class FindGymController implements Initializable {
             HighlightButton.darkColor(signInGym);
         } else if (event.getSource() == home) {
             HighlightButton.darkColor(home);
-        } else if (event.getSource() == submitSignUpGym) {
-            HighlightButton.darkColor(submitSignUpGym);
-        } else if (event.getSource() == skipSignUpGym) {
-            HighlightButton.darkColor(skipSignUpGym);
         } else if (event.getSource() == sportQuiz) {
             HighlightButton.darkColor(sportQuiz);
         } else if (event.getSource() == findGym) {
