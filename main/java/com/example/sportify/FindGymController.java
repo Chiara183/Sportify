6a5ac3027d7e6093b5946c3b5140474e5d713fb1
Matteo <92362045+StitchMl@users.noreply.github.com>
2021-11-15 11:28:44 +0100

    Add FindGym window

--- main/java/com/example/sportify/FindGymController.java
+++ main/java/com/example/sportify/FindGymController.java
@@ -1,19 +1,15 @@
 package com.example.sportify;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 
-import javax.swing.*;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.ResourceBundle;
 
-public class SignUpGymController implements Initializable {
+public class FindGymController implements Initializable {
 
     //TextField
     @FXML
@@ -38,44 +34,6 @@ public class SignUpGymController implements Initializable {
     private Button findGym;
 
     @FXML
-    protected void sportQuizAction() {
-        JFrame jFrame = new JFrame();
-        JOptionPane.showMessageDialog(jFrame, "You're in sport quiz form!");
-    }
-
-    @FXML
-    protected void findGymAction() {
-        JFrame jFrame = new JFrame();
-        JOptionPane.showMessageDialog(jFrame, "You try to find a Gym!");
-    }
-
-    @FXML
-    protected void submitActionSignUpGym(ActionEvent event) throws Exception {
-        if (event.getSource().equals(KeyCode.ENTER) || event.getSource().equals(submitSignUpGym)) {
-            HashMap<String, String[]> account = new HashMap<>();
-            String[] userGymAccount = new String[3];                                //initialize list of string 'userGymAccount'
-            String gymValue = gymName.getText();                                    //get user entered gym name
-            String addressValue = gymAddress.getText();                             //get user entered gym address
-            String cityValue = gymCity.getText();                                   //get user entered gym city
-            userGymAccount[0] = gymValue;                                           //put userValue in userAccount
-            userGymAccount[1] = addressValue;                                       //put user password in userAccount
-            userGymAccount[2] = cityValue;                                          //put user firstName in userAccount
-            account.put(gymValue, userGymAccount);
-
-            //check whether the credentials are authentic or not
-            JFrame jFrame = new JFrame();
-            if (!gymValue.equals("") && !addressValue.equals("") && !cityValue.equals("")) {    //if authentic, navigate user to a new page
-                readWriteFile.saveOnFile(account);
-                JOptionPane.showMessageDialog(jFrame, "You're registered!");
-                signLoginAction();
-            } else {
-                //show error message
-                JOptionPane.showMessageDialog(jFrame, "Please enter all value.");
-            }
-        }
-    }
-
-    @FXML
     protected void signLoginAction() throws Exception {
         CreateWindow.signLoginAction(signInGym);
     }
@@ -84,12 +42,10 @@ public class SignUpGymController implements Initializable {
     private void homeAction() throws Exception {
         CreateWindow.homeAction(home);
     }
-
     @FXML
     private void skipAction() throws Exception {
         CreateWindow.loginAction(skipSignUpGym);
     }
-
     @FXML
     private void lightColor(MouseEvent event) {
         if (event.getSource() == signInGym) {
@@ -129,4 +85,3 @@ public class SignUpGymController implements Initializable {
         // TODO
     }
 }
-
