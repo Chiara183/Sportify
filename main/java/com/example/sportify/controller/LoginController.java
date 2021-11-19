21fdfaa4c85cbe6fa45d93d7087aa3eccf653770
Matteo <92362045+StitchMl@users.noreply.github.com>
2021-11-19 12:10:06 +0100

    Add return Home after Login

--- main/java/com/example/sportify/controller/LoginController.java
+++ main/java/com/example/sportify/controller/LoginController.java
@@ -9,7 +9,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
-import java.io.IOException;
 
 public class LoginController extends ButtonAction implements Initializable {
 
@@ -22,9 +21,11 @@ public class LoginController extends ButtonAction implements Initializable {
     //Button
     @FXML
     private Button skipLogin;
+    @FXML
+    private Button submit;
 
     @FXML
-    protected void submitActionLogin() throws IOException {
+    protected void submitActionLogin() throws Exception {
         String userValue = username.getText();      //get user entered username from the textField1
         String passValue = password.getText();      //get user entered password from the textField2
 
@@ -32,6 +33,7 @@ public class LoginController extends ButtonAction implements Initializable {
         JFrame jFrame = new JFrame();
         if (Submit.login(userValue, passValue)) {   //if authentic, navigate user to a new page
             JOptionPane.showMessageDialog(jFrame, "Correct!");
+            CreateWindow.home(submit);
         } else {
             //show error message
             JOptionPane.showMessageDialog(jFrame, "Please enter valid username and password or Signup");
