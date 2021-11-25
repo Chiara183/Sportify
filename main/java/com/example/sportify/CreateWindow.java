096a45a08224e425436e9e5d7fd16058c6cb72d3
Chiara183 <92364471+Chiara183@users.noreply.github.com>
2021-11-25 10:49:09 +0100

    commit

--- main/java/com/example/sportify/CreateWindow.java
+++ main/java/com/example/sportify/CreateWindow.java
@@ -5,6 +5,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
