4a99e6b366bd5e07aae937cd6f4e5befb5676ec6
Matteo <92362045+StitchMl@users.noreply.github.com>
2021-12-01 19:58:40 +0100

    Commit

--- main/java/com/example/sportify/Home.java
+++ main/java/com/example/sportify/Home.java
@@ -3,6 +3,7 @@ package com.example.sportify;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -21,7 +22,8 @@ public class Home extends Application{
         Scene sceneHome = new Scene(root, 780, 437);
 
         //set stage
-        stage.setTitle("HOME FORM");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("Images/8675330_ic_fluent_sport_regular_icon.png"))));
+        stage.setTitle("Sportify - Home");
         stage.setScene(sceneHome);
         stage.setResizable(false);
         stage.show();
