d8594643bb04617c3385c1f06198fb6dd30ac0be
Matteo <92362045+StitchMl@users.noreply.github.com>
2021-12-16 02:08:02 +0100

    You can find location now!!

--- main/java/com/example/sportify/controller/MapController.java
+++ main/java/com/example/sportify/controller/MapController.java
@@ -1,36 +1,27 @@
 package com.example.sportify.controller;
 
-
 import com.dlsc.gmapsfx.GoogleMapView;
-import com.dlsc.gmapsfx.MapComponentInitializedListener;
 import com.dlsc.gmapsfx.javascript.object.*;
-import com.dlsc.gmapsfx.service.directions.*;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import com.example.sportify.MainApp;
+import com.example.sportify.OpenStreetMapUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
 
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
 
-public class MapController implements Initializable, MapComponentInitializedListener, DirectionsServiceCallback {
-
-    protected DirectionsService directionsService;
-    protected DirectionsPane directionsPane;
-
-    protected StringProperty from = new SimpleStringProperty();
-    protected StringProperty to = new SimpleStringProperty();
-    protected DirectionsRenderer directionsRenderer = null;
+public class MapController implements Initializable{
 
     // Button
     @FXML
-    protected Button clearButton;
-    @FXML
-    protected Button searchButton;
+    private Button searchButton;
 
     // MenuButton
     @FXML
@@ -38,51 +29,71 @@ public class MapController implements Initializable, MapComponentInitializedList
 
     // GoogleMapView
     @FXML
-    protected GoogleMapView mapView;
+    private GoogleMapView mapView;
 
     // TextField
     @FXML
-    protected TextField fromTextField;
-    @FXML
-    protected TextField toTextField;
-    @FXML
-    protected TextField search;
+    private TextField search;
 
+    // WebView
     @FXML
-    private void toTextFieldAction(ActionEvent event) {
-        DirectionsRequest request = new DirectionsRequest(search.getText(), search.getText(), TravelModes.DRIVING, true);
-        directionsRenderer = new DirectionsRenderer(true, mapView.getMap(), directionsPane);
-        directionsService.getRoute(request, this, directionsRenderer);
+    private WebView webMap;
+
+    private GoogleMap map;
+
+    // Reference to the main application.
+    private MainApp mainApp = new MainApp();
+
+    public MapController() {
     }
 
     @FXML
-    private void clearDirections(ActionEvent event) {
-        directionsRenderer.clearDirections();
+    public void searchAction(ActionEvent event) {
+        Map<String, Double> coords;
+        coords = OpenStreetMapUtils.getInstance().getCoordinates(search.getText());
+        System.out.println("latitude :" + coords.get("lat"));
+        System.out.println("longitude:" + coords.get("lon"));
+        if (coords.get("lat") != null && coords.get("lon") != null){
+            LatLong latLong = new LatLong(coords.get("lat"), coords.get("lon"));
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLong)
+                    .title("My new Marker")
+                    .animation(Animation.DROP)
+                    .visible(true);
+            //.icon("my_marker.png")
+
+            Marker myMarker = new Marker(markerOptions);
+            map.setCenter(latLong);
+            map.addMarker(myMarker);
+        }else{
+            //show error message
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("Wrong address");
+            alert.setHeaderText("Sorry, we can't find your address");
+            alert.setContentText("Please enter valid address");
+            alert.showAndWait();
+        }
     }
 
-    @Override
-    public void directionsReceived(DirectionsResult results, DirectionStatus status) {
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        mapView.addMapInitializedListener(this);
-        to.bindBidirectional(search.textProperty());
-        from.bindBidirectional(search.textProperty());
+        mapView.addMapInitializedListener(this::configureMap);
+        //webMap.getEngine().load("https://www.opencyclemap.org/");
     }
 
-    @Override
-    public void mapInitialized() {
-        MapOptions options = new MapOptions();
+    protected void configureMap() {
+        MapOptions mapOptions = new MapOptions();
 
-        options.center(new LatLong(41.902782, 12.496366))
-                .zoomControl(true)
+        mapOptions.center(new LatLong(41.9109, 12.4818))
+                .mapType(MapTypeIdEnum.ROADMAP)
                 .zoom(11)
-                .overviewMapControl(false)
-                .mapType(MapTypeIdEnum.ROADMAP);
-        GoogleMap map = mapView.createMap(options);
-        directionsService = new DirectionsService();
-        directionsPane = mapView.getDirec();
-    }
+                .keyboardShortcuts(false)
+                .clickableIcons(false);
+        map = mapView.createMap(mapOptions, false);
 
+        //map.addMouseEventHandler(UIEventType.click, (GMapMouseEvent event) -> {});
+
+    }
 }
--- main/java/module-info.java
+++ main/java/module-info.java
@@ -6,6 +6,9 @@ module com.example.sportify {
     requires javafx.graphics;
     requires java.desktop;
     requires com.dlsc.gmapsfx;
+    requires org.apache.log4j;
+    requires json.simple;
+    requires org.apache.logging.log4j;
 
     opens com.example.sportify to javafx.fxml;
     exports com.example.sportify;
