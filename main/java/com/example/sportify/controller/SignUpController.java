3dee8c238863e8197e00d06e93ca5e6246cb948a
Matteo <92362045+StitchMl@users.noreply.github.com>
2022-01-18 12:15:17 +0100

    Organize project

--- main/java/com/example/sportify/controller/SignUpController.java
+++ main/java/com/example/sportify/controller/SignUpController.java
@@ -3,47 +3,16 @@ package com.example.sportify.controller;
 import com.example.sportify.MainApp;
 import com.example.sportify.Submit;
 import com.example.sportify.controller.graphic.GraphicController;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import com.example.sportify.controller.graphic.SignUpGraphicController;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.HashMap;
 
 public class SignUpController extends AccessController {
 
-    /** All the text field of the interface*/
-    @FXML
-    private TextField username;
-    @FXML
-    private TextField password;
-    @FXML
-    private TextField firstName;
-    @FXML
-    private TextField lastName;
-    @FXML
-    private TextField email;
-    @FXML
-    private TextField pass_text;
-
-    //Label
-    @FXML
-    private Label eye;
-
-    /** All the checkbox of the interface*/
-    @FXML
-    private CheckBox pass_toggle;
-    @FXML
-    private CheckBox gymTick;
-    @FXML
-    private CheckBox userTick;
+    /** Reference to graphic controller*/
+    private SignUpGraphicController graphicController;
 
     /** The constructor.*/
     public SignUpController() {
@@ -51,107 +20,14 @@ public class SignUpController extends AccessController {
         this.submit = new Submit(null);
     }
 
-    /** Controls the visibility of the password*/
-    @FXML
-    private void set_toggle_pass(){
-        if(!this.pass_toggle.isSelected()) {
-            eye.setStyle("-fx-text-fill: #06B7C5;");
-            pass_toggle.setSelected(true);
-            mainApp.togglevisiblePassword(this.pass_toggle, this.pass_text, this.password);
-        } else {
-            eye.setStyle("-fx-text-fill: black;");
-            pass_toggle.setSelected(false);
-            mainApp.togglevisiblePassword(this.pass_toggle, this.pass_text, this.password);
-        }
-    }
-
-    /** Controls the sign-up method (user, gym)*/
-    @FXML
-    private void tickAction(ActionEvent event) {
-        if (event.getSource() == gymTick && userTick.isSelected()) {
-            userTick.setSelected(false);
-        } else if (event.getSource() == userTick && gymTick.isSelected()) {
-            gymTick.setSelected(false);
-        } else if (event.getSource() == gymTick && !userTick.isSelected()) {
-            userTick.setSelected(true);
-        } else if (event.getSource() == userTick && !gymTick.isSelected()) {
-            gymTick.setSelected(true);
-        }
-    }
-
-    /** The action of the buttons*/
-    @FXML
-    protected void submitActionSignUp() {
-        HashMap<String, String> userAccount = new HashMap<>();  //initialize list of string 'userAccount'
-        String userValue = username.getText();                  //get user entered username
-        String passValue = password.getText();                  //get user entered password
-        String nameValue = firstName.getText();                 //get user entered first name
-        String lastNameValue = lastName.getText();              //get user entered last name
-        String email = this.email.getText();
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        String date = timestamp.toString();
-        date = date.substring(0,10);
-        userAccount.put("username", userValue);                 //put userValue in userAccount
-        userAccount.put("password", passValue);                 //put user password in userAccount
-        userAccount.put("firstName", nameValue);                //put user firstName in userAccount
-        userAccount.put("lastName", lastNameValue);             //put user lastName in userAccount
-        userAccount.put("email", email);                           //put user email in userAccount
-        userAccount.put("birthday", date);                      //put user birthday in userAccount
-
-        //check whether the credentials are authentic or not
-        if (!email.equals("") && !userValue.equals("") && !passValue.equals("") && !submit.exist(userValue) && !submit.existEmail(email)) {    //if authentic, navigate user to a new page
-            if (userTick.isSelected()) {
-                userAccount.put("ruolo", "user");
-                this.submit.signUp(userAccount);
-                JFrame jFrame = new JFrame();
-                JOptionPane.showMessageDialog(jFrame, "You're registered!");
-                login();
-            } else if (gymTick.isSelected()) {
-                userAccount.put("ruolo", "gym");
-                this.submit.signUp(userAccount);
-                signUpGymAction();
-            }
-        } else {
-            if (submit.exist(userValue)){
-                //show error message
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.initOwner(mainApp.getPrimaryStage());
-                alert.setTitle("User already exists");
-                alert.setHeaderText("The user already exists");
-                alert.setContentText("Please enter a different username or login.");
-                alert.showAndWait();
-            } else if (submit.existEmail(email)){
-                //show error message
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.initOwner(mainApp.getPrimaryStage());
-                alert.setTitle("User already exists");
-                alert.setHeaderText("The email is already registered");
-                alert.setContentText("Please enter a different email or login.");
-                alert.showAndWait();
-            } else {
-                //show error message
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.initOwner(mainApp.getPrimaryStage());
-                alert.setTitle("Empty field");
-                alert.setHeaderText("Some obligatory value are empty");
-                alert.setContentText("Please enter all obligatory value.");
-                alert.showAndWait();
-            }
-        }
-    }
-    @FXML
-    private void skipAction(){
-        login();
-    }
-
     /** It's called to load login overview*/
-    private void login(){
+    public void login(){
         this.mainApp.setSubmit(this.submit);
         this.mainApp.showLoginOverview();
     }
 
     /** It's called to load sign up gym overview*/
-    private void signUpGymAction(){
+    public void signUpGymAction(){
         this.mainApp.getPrimaryStage().setTitle("Sportify - Sign Up");
         try {
             // Load login overview.
@@ -174,7 +50,7 @@ public class SignUpController extends AccessController {
 
     @Override
     public void setGraphicController(GraphicController graphicController) {
-        //TODO
+        this.graphicController = (SignUpGraphicController) graphicController;
     }
 }
 
