958c9a699f2e1a461a9c2cdd5a0fa1c215f8bab5
Chiara183 <92364471+Chiara183@users.noreply.github.com>
2022-02-07 10:42:30 +0100

    commit

---
+++ main/java/com/example/sportify/controller/graphicPhone/SportQuizPhoneGraphicController.java
@@ -0,0 +1,15 @@
+package com.example.sportify.controller.graphicPhone;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SportQuizPhoneGraphicController {
+
+
+    @FXML
+    private TextField age;
+    @FXML
+    private Button okButton;
+
+}
