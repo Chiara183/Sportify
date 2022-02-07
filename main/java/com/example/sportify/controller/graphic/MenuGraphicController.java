76add0e0a76b1de6cf3ecb9606c03a98952c293c
Chiara183 <92364471+Chiara183@users.noreply.github.com>
2022-02-07 10:14:09 +0100

    commit

--- main/java/com/example/sportify/controller/graphic/MenuGraphicController.java
+++ main/java/com/example/sportify/controller/graphic/MenuGraphicController.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
