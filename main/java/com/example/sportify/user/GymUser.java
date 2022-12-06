4ee44d71ce5beef988cf2efe5216bd92db11c620
Matteo <92362045+StitchMl@users.noreply.github.com>
2022-12-06 15:35:21 +0100

    Upgrade version

--- main/java/com/example/sportify/user/GymUser.java
+++ main/java/com/example/sportify/user/GymUser.java
@@ -5,8 +5,6 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class GymUser extends User {
 
-    private static final String WHERE = "' WHERE `gym`.`owner` = '";
-
     /** The constructor.*/
     public GymUser() {
         this(null, null);
@@ -24,42 +22,42 @@ public class GymUser extends User {
     /** The method that set up gym variable*/
     public void setGymName(String name) {
         this.gymName.set(name);
-        DAO objDAO = mainApp.getDAO();
-        objDAO.updateDB(
+        DAO obj_DAO = mainApp.getDAO();
+        obj_DAO.updateDB(
                 "UPDATE `gym` SET `name` = '"
-                        + name + WHERE
+                        + name + "' WHERE `gym`.`owner` = '"
                         + this.userName.getValue() +"'");
     }
     public void setAddress(String address) {
         this.address.set(address);
-        DAO objDAO = mainApp.getDAO();
-        objDAO.updateDB(
+        DAO obj_DAO = mainApp.getDAO();
+        obj_DAO.updateDB(
                 "UPDATE `gym` SET `address` = '"
-                        + address + WHERE
+                        + address + "' WHERE `gym`.`owner` = '"
                         + this.userName.getValue() +"'");
     }
     public void setLatitude(String latitude) {
         this.latitude.set(latitude);
-        DAO objDAO = mainApp.getDAO();
-        objDAO.updateDB(
+        DAO obj_DAO = mainApp.getDAO();
+        obj_DAO.updateDB(
                 "UPDATE `gym` SET `latitude` = '"
-                        + latitude + WHERE
+                        + latitude + "' WHERE `gym`.`owner` = '"
                         + this.userName.getValue() +"'");
     }
     public void setLongitude(String longitude) {
         this.longitude.set(longitude);
-        DAO objDAO = mainApp.getDAO();
-        objDAO.updateDB(
+        DAO obj_DAO = mainApp.getDAO();
+        obj_DAO.updateDB(
                 "UPDATE `gym` SET `longitude` = '"
-                        + longitude + WHERE
+                        + longitude + "' WHERE `gym`.`owner` = '"
                         + this.userName.getValue() +"'");
     }
     public void setPhone(String phone) {
         this.phone.set(phone);
-        DAO objDAO = mainApp.getDAO();
-        objDAO.updateDB(
+        DAO obj_DAO = mainApp.getDAO();
+        obj_DAO.updateDB(
                 "UPDATE `gym` SET `phone` = '"
-                        + phone + WHERE
+                        + phone + "' WHERE `gym`.`owner` = '"
                         + this.userName.getValue() +"'");
     }
 }
