ccd6e6a691a37874d10145a5e1650fd82df4bca8
Matteo <92362045+StitchMl@users.noreply.github.com>
2022-12-29 15:06:57 +0100

    Resolve Code Smells

--- src/test/java/selenium/testBaseSelenium.java
+++ src/test/java/selenium/testBaseSelenium.java
@@ -1,19 +1,16 @@
 package selenium;
 
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public abstract class testBaseSelenium {
 
-    private static final Logger LOGGER = Logger.getLogger(testBaseSelenium.class.getName());
-
     protected static void sleep(long duration) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(duration);
-        } catch (InterruptedException e) {
-            LOGGER.log(Level.WARNING, "Error in Thread sleep!");
-        }
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {}
+        }, duration);
     }
 
 }
