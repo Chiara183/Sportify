894d68cbc0347bc831571089b1dd9ac52feda589
Matteo <92362045+StitchMl@users.noreply.github.com>
2022-12-29 15:44:38 +0100

    Resolve Code Smells

--- src/test/java/sportify/HomePhoneTest.java
+++ src/test/java/sportify/HomePhoneTest.java
@@ -3,8 +3,9 @@ package sportify;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.util.WaitForAsyncUtils;
@@ -12,7 +13,7 @@ import org.testfx.util.WaitForAsyncUtils;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(ApplicationExtension.class)
-class HomePhoneTest  extends StartingTest{
+class HomePhoneTest extends StartingTest{
 
     private static final String COMBOBOX = "#comboActivity";
 
@@ -22,27 +23,16 @@ class HomePhoneTest  extends StartingTest{
         WaitForAsyncUtils.waitForFxEvents(100);
     }
 
-    @Test
-    void openSportQuizTest() {
-        robot.clickOn(COMBOBOX).write("Take sport quiz").press(KeyCode.ENTER).release(KeyCode.ENTER);
+    @ParameterizedTest
+    @CsvSource({
+            "Take sport quiz, Sportify - Sport Quiz",
+            "Login, Sportify - Login",
+            "Find gym, Sportify - Find Gym",
+    })
+    void openSceneTest(String wr, String t) {
+        robot.clickOn(COMBOBOX).write(wr).press(KeyCode.ENTER).release(KeyCode.ENTER);
         Stage stage = FxToolkit.toolkitContext().getRegisteredStage();
         String title = stage.getTitle();
-        assertEquals("Sportify - Sport Quiz", title);
-    }
-
-    @Test
-    void openLoginWindow() {
-        robot.clickOn(COMBOBOX).write("Login").press(KeyCode.ENTER).release(KeyCode.ENTER);
-        Stage stage = FxToolkit.toolkitContext().getRegisteredStage();
-        String title = stage.getTitle();
-        assertEquals("Sportify - Login", title);
-    }
-
-    @Test
-    void openGymFinderWindow() {
-        robot.clickOn(COMBOBOX).write("Find gym").press(KeyCode.ENTER).release(KeyCode.ENTER);
-        Stage stage = FxToolkit.toolkitContext().getRegisteredStage();
-        String title = stage.getTitle();
-        assertEquals("Sportify - Find Gym", title);
+        assertEquals(t, title);
     }
 }
